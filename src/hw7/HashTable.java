0789abe3c30128b7dc2d9fe63a6b5ff7d6c41d4f
Mike Shilovski <ShMike6491@gmail.com>
2020-12-22 12:38:50 +0300

    HashTables

---
+++ src/hw7/HashTable.java
@@ -0,0 +1,16 @@
+package hw7;
+
+public interface HashTable {
+
+    boolean put(Item item, Integer cost);
+
+    Integer get(Item item);
+
+    boolean remove(Item item);
+
+    int size();
+
+    boolean isEmpty();
+
+    void display();
+}
